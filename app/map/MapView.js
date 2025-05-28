5febc714ace0d6794638abaeacab3a390e1f4924
metehansaral <metehansaral@gmail.com>
2025-05-28 21:07:58 +0200

    added campus locations

--- app/map/MapView.js
+++ app/map/MapView.js
@@ -18,6 +18,31 @@ const userIcon = new L.Icon({
   popupAnchor: [0, -32],
 });
 
+const altinbasIcon = new L.Icon({
+  iconUrl: "/altinbas.png",
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
+});
+
+const campusPins = [
+  {
+    position: [41.06995077773232, 29.01285636408775],
+    name: "Gayrettepe Kampüsü",
+    color: "#0074D9", // Mavi
+  },
+  {
+    position: [41.05740047410192, 28.820389963048207],
+    name: "Mahmutbey Kampüsü",
+    color: "#FF4136", // Kırmızı
+  },
+  {
+    position: [40.98309094546604, 28.872312012309017],
+    name: "Bakırköy Kampüsü",
+    color: "#145A32", // Koyu Yeşil
+  },
+];
+
 function RecenterMap({ center }) {
   const map = useMap();
   const hasCenteredRef = React.useRef(false);
@@ -43,6 +68,14 @@ export default function MapView({ center, buses, userLocation, showUserPin = tru
       {buses.map((bus) => (
         <Marker key={bus.name} position={[bus.lat, bus.lng]} icon={busIcon}>
           <Popup>{bus.name}</Popup>
+        </Marker>
+      ))}
+      {/* Campus Pins */}
+      {campusPins.map((campus, idx) => (
+        <Marker key={campus.name} position={campus.position} icon={altinbasIcon}>
+          <Popup>
+            <span style={{ color: campus.color, fontWeight: 'bold' }}>{campus.name}</span>
+          </Popup>
         </Marker>
       ))}
       {userLocation && showUserPin && (
